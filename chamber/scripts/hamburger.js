49895033cea54b17152b4e256d65a0f9fe504ace
herbertmoroni <herbertmoroni@hotmail.com>
2024-05-18 10:54:32 -0600

    first version

---
+++ chamber/scripts/hamburger.js
@@ -0,0 +1,8 @@
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', function() {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
+
