5e24c7fe9ac31ef9d74dc87959277bf7522b7ef5
herbertmoroni <herbertmoroni@hotmail.com>
2024-06-28 21:36:56 -0600

    A banner appears on Monday, Tuesday, and Wednesdays only

--- chamber/scripts/banner.js
+++ chamber/scripts/banner.js
@@ -1,7 +1,7 @@
 function displayBanner() {
     const banner = document.getElementById('banner');
     const closeButton = document.getElementById('close-banner');
-    const dayOfWeek = 1 //new Date().getDay(); // 0 is Sunday, 1 is Monday, etc.
+    const dayOfWeek = new Date().getDay(); // 0 is Sunday, 1 is Monday, etc.
 
     if (dayOfWeek >= 1 && dayOfWeek <= 3) { // Monday to Wednesday
         banner.classList.add('active');
